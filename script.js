86b701fff7ebec90380aa47a7c20eb1fc55b6338
Uģis Krūkliņš <138369039+ufssuk@users.noreply.github.com>
2023-08-17 22:46:17 +0300

    Pievienojām HTML elementus

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 const plusins = document.querySelector('.js-plus')
-const inputins = document.querySelector('.js-input')
+const multiply = document.querySelector ('.js-multiply')
+const minus = document.querySelector ('.js-mminus')
+const divide = document.querySelector ('.js-divide')
+const inputins1 = document.querySelector('.js-input1')
+const inputins2 = document.querySelector('.js-input2')
+const answer = document.querySelector('js-result')
 
-console.log(plusins)
-console.log(inputins)
+let firstValue = inputins1.value
+let secondValue = inputins2.value
 
+const Add = (cipars1,cipars2) => {
+    return cipars1 + cipars2
+}
 
+const Subtract = (cipars1,cipars2) => {
+    return cipars1 - cipars2
+}
+
+const Multiply = (cipars1,cipars2) => {
+    return cipars1 * cipars2
+}
+
+const Divide = (cipars1,cipars2) => {
+    return cipars1 / cipars2
+}
+
+console.log(Add(2,3))
+
+plusins.addEventListener('click',() => {
+    console.log(Add(firstValue,secondValue))
+})
