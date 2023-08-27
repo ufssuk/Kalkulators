1e3492c256d70b8adec4b8d0b0fa6bed0e6b7cc0
Uģis Krūkliņš <138369039+ufssuk@users.noreply.github.com>
2023-08-27 12:26:26 +0300

    Sākām stilot mūsu projektu

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const plusins = document.querySelector('.js-plus')
 const multiply = document.querySelector ('.js-multiply')
-const minus = document.querySelector ('.js-mminus')
+const minus = document.querySelector ('.js-minus')
 const divide = document.querySelector ('.js-divide')
 const inputins1 = document.querySelector('.js-input1')
 const inputins2 = document.querySelector('.js-input2')
@@ -22,12 +22,27 @@ const Divide = (cipars1,cipars2) => {
     return cipars1 / cipars2
 }
 
-// console.log(Add(2,3))
+const clear =() => {
+    inputins1.value = ""
+    inputins2.value = ""
+}
 
 plusins.addEventListener('click',() => {
     answer.textContent = Add(parseInt(inputins1.value),parseInt(inputins2.value))
 
-    inputins1.value = ""
-    inputins2.value = ""
+    clear()
 })
+
+multiply.addEventListener('click',() => {
+    answer.textContent = Multiply(parseInt(inputins1.value),parseInt(inputins2.value))
+
+    clear()
+})
+
+divide.addEventListener('click',() => {
+    answer.textContent = Divide(parseInt(inputins1.value),parseInt(inputins2.value))
+
+    clear()
+})
+
 
